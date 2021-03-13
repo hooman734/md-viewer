9e1679b1d05a679c6ca4d0dbf8e6447920b0564d
Hooman <hesamyan@outlook.com>
2021-03-14 01:07:09 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import MDTranslatorCP from "./components/MDTranslatorCP";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = function () {
+
+
+    return (
+        <React.Fragment>
+            <h5 className="mx-5 my-2 py-2 text-muted">This application meant to simply interpret and then view your <span className="text-info" title="Markdown is a lightweight markup language for creating formatted text using a plain-text editor. John Gruber and Aaron Swartz created Markdown in 2004 as a markup language that is appealing to human readers in its source code form.">markdown</span> input.</h5>
+            <MDTranslatorCP/>
+        </React.Fragment>
+    );
 }
 
 export default App;
